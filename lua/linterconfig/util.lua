3d896e368d06634c8066ac34f609165b28f6fbfb
skeletony007 <skeletony007@users.noreply.github.com>
2025-11-14 14:59:27 +0000

    fix(util): make it work without root markers

--- lua/linterconfig/util.lua
+++ lua/linterconfig/util.lua
@@ -2,6 +2,10 @@ local M = {}
 
 M.default_config = {
     autostart = function(self, source)
+        if self.root_markers == nil then
+            return true
+        end
+
         local directory = vim.fs.root(source, self.root_markers)
         if directory == nil then
             return false
